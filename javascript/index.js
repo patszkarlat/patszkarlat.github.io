d33d840383f0923518578963c089b760969c507d
patszkarlat <punkgeek@mac.com>
2014-10-03 14:30:50 -0400

    fixed the contact div

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+$('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 1100,
+        onScroll: function(scrollPos){
+            // scrollPos:Number
+        },
+        onSnapComplete: function($el){
+            // after window ($el) snaps into place
+        },
+        onWindowEnter: function($el){
+            // when new window ($el) enters viewport
+        }
+    });
